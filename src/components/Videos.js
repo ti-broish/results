7a7484b2d98b347961822b82eb7870e2f0776994
Blagoslav Mihaylov <az@blago.io>
2021-07-11 21:20:38 +0300

    Improve stream section

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -16,7 +16,7 @@ import styled from 'styled-components';
 import { Fade } from 'react-reveal';
 
 const Wrapper = styled.div`
-  height: 70vh;
+  height: 35vh;
   border-collapse: collapse;
   box-shadow: rgb(170 170 170) 0px 0px 10px;
   margin-bottom: 20px;
@@ -25,6 +25,21 @@ const Wrapper = styled.div`
 const StyledPlayer = styled.div`
   width: 100%;
   height: 100%;
+
+  .vjs-big-play-button {
+    right: 0;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    margin: auto;
+    display: block;
+    position: absolute;
+  }
+`;
+
+const LiveStreams = styled.div`
+    max-width: 500px;
+    margin: 0 auto;
 `;
 
 class VideoPlayer extends React.Component {
@@ -78,6 +93,38 @@ class VideoPlayer extends React.Component {
     }
   }
 
+const ShowMoreButton = styled.button`
+  cursor: pointer;
+  width: 300px;
+  margin: 0 auto;
+  display: block;
+  padding: 13px;
+  border: 1px solid #ccc;
+  background-color: #999;
+  color: white;
+  font-size: 17px;
+  font-weight: bold;
+  border-radius: 10px;
+  border-top: 0px;
+  border-bottom: 5px solid #666;
+
+  &:hover {
+      background-color: #aaa;
+  }
+
+  &:disabled {
+      background-color: #888;
+      color: #bbb;
+      border-top: 5px solid white;
+      border-bottom: 0px;
+  }
+
+  &:active {
+      border-top: 5px solid white;
+      border-bottom: 0px;
+  }
+`;
+
 export default props => {
     const { meta, parties, dataURL } = useContext(ElectionContext);
     const [data, setData] = useState({
@@ -90,14 +137,26 @@ export default props => {
         axios.get(`${dataURL}/streams/feed`).then(res => {
             setData({
                 items: res.data,
-                moreToLoad: res.data.length >= 50
+                moreToLoad: res.data.length >= 5
             });
         });
     }, []);
 
+    const getMoreStreams = () => {
+        setLoading(true);
+        const lastViolation = data.items[data.items.length-1].id;
+        axios.get(`${dataURL}/streams/feed?after=${lastViolation}`).then(res => {
+            setData({
+                items: [...data.items, ...res.data],
+                moreToLoad: res.data.length >= 5
+            });
+            setLoading(false);
+        });
+    };
+
     const renderStream = (stream, i) => {
 
-        console.log(stream);
+        console.log(stream.section);
 
         const videoJsOptions = {
             autoplay: false,
@@ -112,32 +171,48 @@ export default props => {
         };
 
         return(
-            <Wrapper>
-                <VideoPlayer
-                    key={i}
-                    {...videoJsOptions}
-                    playlist={stream.chunks.map((source) => {
-                    return {
-                        sources: [{ src: source.url, type: "video/mp4" }],
-                        poster: "",
-                    };
-                    })}
-                    chunks={stream.chunks}
-                    isStreaming={stream.isStreaming}
-                    index={i}
-                    broadcastUrl={stream.broadcastUrl}
-                />
-            </Wrapper>
+            <>
+                <h3 style={{margin: '20px 0 10px 0'}}>
+                    <Link to={`/${stream.section.id}`}>Секция {stream.section.id}</Link>, 
+                    {' '}
+                    МИР {stream.section.electionRegion.code}. {stream.section.electionRegion.name}
+                </h3>
+                <h5 style={{color: '#666', marginTop: '10px'}}>
+                    Град {stream.section.town.name}, {stream.section.place}
+                </h5>
+                <Wrapper>
+                    <VideoPlayer
+                        key={i}
+                        {...videoJsOptions}
+                        playlist={stream.chunks.map((source) => {
+                        return {
+                            sources: [{ src: source.url, type: "video/mp4" }],
+                            poster: "",
+                        };
+                        })}
+                        chunks={stream.chunks}
+                        isStreaming={stream.isStreaming}
+                        index={i}
+                        broadcastUrl={stream.broadcastUrl}
+                    />
+                </Wrapper>
+            </>
         );
     };
 
     return(
-        <>
-            <h1>Видеа</h1>
+        <LiveStreams>
+            <h1 style={{textAlign: 'center'}}>Стриймове от секциите</h1>
             {
                 !data.items? <LoadingScreen/> :
                     data.items.map(renderStream)
             }
-        </>
+            {
+                !data.moreToLoad? null :
+                    <ShowMoreButton disabled={loading} onClick={getMoreStreams}>
+                        {loading? 'Зареждане...' : 'Покажи още'}
+                    </ShowMoreButton>
+            }
+        </LiveStreams>
     );
 };
