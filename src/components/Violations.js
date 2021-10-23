b52e6b77a2ca122e706285b6831ab60c9dd2c2d4
Radoslav Popov <rpopov@slashwhy.de>
2021-10-23 15:01:58 +0200

    clean more warnings

--- src/components/Violations.js
+++ src/components/Violations.js
@@ -12,137 +12,158 @@ import styled from 'styled-components';
 import { Fade } from 'react-reveal';
 
 const ViolationFeed = styled.div`
-    max-width: 600px;
-    margin: 0 auto; 
-    padding: 10px;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 10px;
 `;
 
 const Violation = styled.div`
-    border: 1px solid #ccc;
-    margin: 15px 0;
-    border-radius: 10px;
-    padding: 10px 25px;
-    box-shadow: 0 0 10px #ddd;
-    border-bottom: 3px solid #bbb;
-    color: #333;
+  border: 1px solid #ccc;
+  margin: 15px 0;
+  border-radius: 10px;
+  padding: 10px 25px;
+  box-shadow: 0 0 10px #ddd;
+  border-bottom: 3px solid #bbb;
+  color: #333;
 
-    h4, p {
-        margin: 5px 0;
-    }
+  h4,
+  p {
+    margin: 5px 0;
+  }
 `;
 
 const ShowMoreButton = styled.button`
-    cursor: pointer;
-    width: 300px;
-    margin: 0 auto;
-    display: block;
-    padding: 13px;
-    border: 1px solid #ccc;
-    background-color: #999;
-    color: white;
-    font-size: 17px;
-    font-weight: bold;
-    border-radius: 10px;
-    border-top: 0px;
-    border-bottom: 5px solid #666;
+  cursor: pointer;
+  width: 300px;
+  margin: 0 auto;
+  display: block;
+  padding: 13px;
+  border: 1px solid #ccc;
+  background-color: #999;
+  color: white;
+  font-size: 17px;
+  font-weight: bold;
+  border-radius: 10px;
+  border-top: 0px;
+  border-bottom: 5px solid #666;
 
-    &:hover {
-        background-color: #aaa;
-    }
+  &:hover {
+    background-color: #aaa;
+  }
 
-    &:disabled {
-        background-color: #888;
-        color: #bbb;
-        border-top: 5px solid white;
-        border-bottom: 0px;
-    }
+  &:disabled {
+    background-color: #888;
+    color: #bbb;
+    border-top: 5px solid white;
+    border-bottom: 0px;
+  }
 
-    &:active {
-        border-top: 5px solid white;
-        border-bottom: 0px;
-    }
+  &:active {
+    border-top: 5px solid white;
+    border-bottom: 0px;
+  }
 `;
 
-export default props => {
-    const { meta, parties, dataURL } = useContext(ElectionContext);
-    const [data, setData] = useState({
-        items: null,
-        moreToLoad: true,
+export default (props) => {
+  const { meta, parties, dataURL } = useContext(ElectionContext);
+  const [data, setData] = useState({
+    items: null,
+    moreToLoad: true,
+  });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    axios.get(`${dataURL}/violations/feed`).then((res) => {
+      console.log(res.data);
+      setData({
+        items: res.data,
+        moreToLoad: res.data.length >= 50,
+      });
     });
-    const [loading, setLoading] = useState(false);
+  }, []);
 
-    useEffect(() => {
-        axios.get(`${dataURL}/violations/feed`).then(res => {
-            console.log(res.data);
-            setData({
-                items: res.data,
-                moreToLoad: res.data.length >= 50
-            });
+  const getMoreViolations = () => {
+    setLoading(true);
+    const lastViolation = data.items[data.items.length - 1].id;
+    axios
+      .get(`${dataURL}/violations/feed?after=${lastViolation}`)
+      .then((res) => {
+        setData({
+          items: [...data.items, ...res.data],
+          moreToLoad: res.data.length >= 50,
         });
-    }, []);
+        setLoading(false);
+      });
+  };
 
-    const getMoreViolations = () => {
-        setLoading(true);
-        const lastViolation = data.items[data.items.length-1].id;
-        axios.get(`${dataURL}/violations/feed?after=${lastViolation}`).then(res => {
-            setData({
-                items: [...data.items, ...res.data],
-                moreToLoad: res.data.length >= 50
-            });
-            setLoading(false);
-        });
-    };
+  const renderViolation = (violation, i) => {
+    let electionRegion = null;
+    if (violation.section) {
+      electionRegion = violation.section.electionRegion;
+    } else if (
+      violation.town.electionRegions &&
+      violation.town.electionRegions.length === 1
+    ) {
+      electionRegion = violation.town.electionRegions[0];
+    }
+    //if section show
 
-    const renderViolation = (violation, i) => {
-        let electionRegion = null;
-        if(violation.section) {
-            electionRegion = violation.section.electionRegion;
-        } else if(violation.town.electionRegions && violation.town.electionRegions.length === 1) {
-            electionRegion = violation.town.electionRegions[0];
-        }
-        //if section show 
-        
-        return(
-            <Fade clear>
-                <Violation key={violation.id}>
-                    <h4>
-                        {violation.town.name}
-                        {violation.town.municipality? ', Община ' + violation.town.municipality.name : ''}
-                        {violation.town.country && violation.town.country.isAbroad? ', ' + violation.town.country.name : ''}
-                    </h4>
-                    <h4>
-                        {!electionRegion? '' : 
-                            <Link to={`/${electionRegion.code}`}>
-                                МИР {electionRegion.code}. {electionRegion.name}
-                            </Link>
-                        }
-                        {electionRegion && violation.section? ', ' : ''}
-                        {!violation.section? '' :
-                            <>
-                                Секция <Link to={`/${violation.section.id}`}>{violation.section.id}</Link>
-                            </>
-                        }
-                    </h4>
-                    {!violation.section? null : <h5 style={{margin: '5px 0'}}>{violation.section.place}</h5>}
-                    <h5 style={{color: '#888', margin: '5px 0'}}>Получен: {formatDateTime(new Date(violation.createdAt))}</h5>
-                    
-                    <p style={{margin: '20px 0 20px 0'}}>{violation.publishedText}</p>
-                </Violation>
-            </Fade>
-        );
-    };
+    return (
+      <Fade key={violation.id} clear>
+        <Violation>
+          <h4>
+            {violation.town.name}
+            {violation.town.municipality
+              ? ', Община ' + violation.town.municipality.name
+              : ''}
+            {violation.town.country && violation.town.country.isAbroad
+              ? ', ' + violation.town.country.name
+              : ''}
+          </h4>
+          <h4>
+            {!electionRegion ? (
+              ''
+            ) : (
+              <Link to={`/${electionRegion.code}`}>
+                МИР {electionRegion.code}. {electionRegion.name}
+              </Link>
+            )}
+            {electionRegion && violation.section ? ', ' : ''}
+            {!violation.section ? (
+              ''
+            ) : (
+              <>
+                Секция{' '}
+                <Link to={`/${violation.section.id}`}>
+                  {violation.section.id}
+                </Link>
+              </>
+            )}
+          </h4>
+          {!violation.section ? null : (
+            <h5 style={{ margin: '5px 0' }}>{violation.section.place}</h5>
+          )}
+          <h5 style={{ color: '#888', margin: '5px 0' }}>
+            Получен: {formatDateTime(new Date(violation.createdAt))}
+          </h5>
 
-    return(
-        !data.items? <LoadingScreen/> :
-        <ViolationFeed>
-            <h1 style={{textAlign: 'center'}}>Последни сигнали</h1>
-            {data.items.map(renderViolation)}
-            {
-                !data.moreToLoad? null :
-                    <ShowMoreButton disabled={loading} onClick={getMoreViolations}>
-                        {loading? 'Зареждане...' : 'Покажи още'}
-                    </ShowMoreButton>
-            }
-        </ViolationFeed>
+          <p style={{ margin: '20px 0 20px 0' }}>{violation.publishedText}</p>
+        </Violation>
+      </Fade>
     );
+  };
+
+  return !data.items ? (
+    <LoadingScreen />
+  ) : (
+    <ViolationFeed>
+      <h1 style={{ textAlign: 'center' }}>Последни сигнали</h1>
+      {data.items.map(renderViolation)}
+      {!data.moreToLoad ? null : (
+        <ShowMoreButton disabled={loading} onClick={getMoreViolations}>
+          {loading ? 'Зареждане...' : 'Покажи още'}
+        </ShowMoreButton>
+      )}
+    </ViolationFeed>
+  );
 };
