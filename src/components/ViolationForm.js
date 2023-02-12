1ae8be4c8627fc440e60c74bb4643ad7971d4df4
konstantinabl <konstantina.blajukova@gmail.com>
2023-02-12 20:35:40 +0200

    Adds dynamic dropdowsn for election regions, minucipalities and towns

--- src/components/ViolationForm.js
+++ src/components/ViolationForm.js
@@ -43,38 +43,85 @@ export default function ViolationForm() {
   } = useForm();
 
   const [electionRegions, setElectionRegions] = useState([]);
-  //   useEffect(() => {
-  //     axios
-  //       .get('http://localhost:4000/election_regions')
-  //       .then((res) => setElectionRegions(res.data));
+  const [selectedElectionRegion, setSelectedElectionRegion] = useState('');
+  const [selectedMunicipality, setSelectedMunicipality] = useState('');
+  const [towns, setTowns] = useState([]);
 
-  //     // return election_regions.map((election_region) => {
-  //     //   return <option value={country.dial_code}>{country.name}</option>;
-  //     // });
-  //   }, []);
+  const api_endpoint = 'http://localhost:4000';
+
+  useEffect(() => {
+    console.log(selectedElectionRegion);
+    axios
+      .get(`${api_endpoint}/election_regions`)
+      .then((res) => setElectionRegions(res.data));
+  }, []);
+
+  useEffect(() => {
+    console.log('in 2nd use effect before if');
+    if (selectedElectionRegion != '') {
+      console.log('get municipalities');
+      getMunicipalities(selectedElectionRegion);
+    }
+    if (selectedElectionRegion != '' && selectedMunicipality != '') {
+      console.log('in 2nd use effect after if');
+      axios
+        .get(
+          `${api_endpoint}/towns?country=00&election_region=${selectedElectionRegion}&municipality=${selectedMunicipality}`
+        )
+        .then((res) => setTowns(res.data));
+    }
+  }, [selectedElectionRegion, selectedMunicipality]);
   const getElectionRegions = () => {
     return electionRegions.map((election_region) => {
-      return <option value={election_region.id}>{election_region.name}</option>;
+      return (
+        <option key={election_region.code} value={election_region.code}>
+          {election_region.name}
+        </option>
+      );
+    });
+  };
+  const getMunicipalities = (selectedElectionRegion) => {
+    console.log('here');
+
+    const filteredRegions = electionRegions.filter(
+      (electionRegion) => electionRegion.code == selectedElectionRegion
+    );
+    const municipalities = [];
+    filteredRegions[0].municipalities.forEach((municipality) => {
+      municipalities.push(municipality);
+    });
+
+    return municipalities.map((municipality) => {
+      return (
+        <option key={municipality.code} value={municipality.code}>
+          {municipality.name}
+        </option>
+      );
+    });
+  };
+  const createTownOptions = () => {
+    console.log('in create town');
+    return towns.map((town) => {
+      return (
+        <option id={town.code} key={town.code} value={town.code}>
+          {town.name}
+        </option>
+      );
     });
   };
   const onSubmit = (data) => {
-    console.log(data);
     const body = {
       description: data.description,
       town: 68134,
     };
     console.log(JSON.stringify(body));
-    // fetch('http://localhost:4000/violations', {
-    //   method: 'POST',
-    //   mode: 'cors',
-    //   body: JSON.stringify(body),
-    // }).then((res) => console.log(res));
     axios
-      .post('http://localhost:4000/violations', body, {
+      .post(`${api_endpoint}/violations`, body, {
         headers: {
           'Content-Type': 'application/json',
         },
       })
+      .then((res) => console.log(res.data))
       .catch((err) => console.log(err));
   };
 
@@ -98,11 +145,48 @@ export default function ViolationForm() {
         </div>
       </div>
       <div>
-        <select className="form-control">
+        <label>МИР</label>
+      </div>
+      <div>
+        <select
+          className="form-control"
+          onChange={(e) => setSelectedElectionRegion(e.target.value)}
+        >
           <option value="choose" disabled selected="selected">
             -- МИР --
           </option>
           {getElectionRegions()}
+        </select>
+      </div>
+      <div>
+        <label>Община</label>
+      </div>
+      <div>
+        <select
+          className="form-control"
+          onChange={(e) => setSelectedMunicipality(e.target.value)}
+        >
+          {selectedElectionRegion != '' ? (
+            getMunicipalities(selectedElectionRegion)
+          ) : (
+            <option value="choose" disabled selected="selected">
+              -- Община --
+            </option>
+          )}
+        </select>
+      </div>
+      <div>
+        <label>Град/село</label>
+      </div>
+      <div>
+        <select className="form-control">
+          {towns.length != 0 ? (
+            createTownOptions()
+          ) : (
+            <option value="choose" disabled selected="selected">
+              -- Градове --
+            </option>
+          )}
         </select>
       </div>
       <div className="form-control">
@@ -137,14 +221,6 @@ export default function ViolationForm() {
         {errors.phoneNumber && errors.phoneNumber.type === 'required' && (
           <p className="errorMsg">Полето е задължително.</p>
         )}
-      </div>
-      <div className="form-control">
-        <label>Град</label>
-        <input
-          type="text"
-          name="city"
-          {...register('city', { required: false })}
-        />
       </div>
       <div className="form-control">
         <label>Секция</label>
