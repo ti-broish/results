5a75bef153ac51ad18bd3984e13982140eb5da67
Haralan Dobrev <harry@hkdobrev.com>
2023-03-03 00:31:29 +0200

    Simplify build process

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -55,6 +55,13 @@ module.exports = env => {
                 template: 'src/index.ejs',
                 publicPath: env['PUBLIC_URL']? env['PUBLIC_URL'] : '/'
             }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+                    'PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL),
+                    'DATA_URL': JSON.stringify(process.env.DATA_URL),
+                },
+            }),
         ],
     }
 };
