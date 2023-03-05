89e5401865508ca38f6774407b1c6911ff9cf3a2
konstantinabl <konstantina.blajukova@gmail.com>
2023-03-05 22:38:29 +0200

    Adds size check for pictures upload

--- src/utils/uploadPhotosHelper.js
+++ src/utils/uploadPhotosHelper.js
@@ -3,13 +3,18 @@ import convertToBase64 from './fileToBase64'
 
 const saveImages = async function (images) {
   let imageIds = []
+  const byteSize = (str) => new Blob([str]).size
   for (const base64Image in images) {
+    const imageMB = byteSize(images[base64Image]) / Math.pow(1024, 2)
     try {
+      if (Math.round(imageMB) > 50) {
+        throw new Error('Размерът на файла е твърде голям')
+      }
       let savedImage = await api.post('pictures', {
         image: images[base64Image],
       })
       imageIds.push(savedImage.id)
-    } catch (_) {
+    } catch (error) {
       console.error(error)
     }
   }
@@ -18,8 +23,9 @@ const saveImages = async function (images) {
 }
 
 const convertImagesToBase64 = async function (images) {
+  const imageArray = Array.from(images)
   return await Promise.all(
-    images.map(async (image) => {
+    imageArray.map(async (image) => {
       return await convertToBase64(image)
     })
   )
