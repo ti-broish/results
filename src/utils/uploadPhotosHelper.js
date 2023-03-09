901b282dfb9dd3eb3c38af5e923a46534582c5d8
konstantinabl <konstantina.blajukova@gmail.com>
2023-03-09 21:27:04 +0200

    Makes necessary changes to use new component

--- src/utils/uploadPhotosHelper.js
+++ src/utils/uploadPhotosHelper.js
@@ -4,14 +4,15 @@ import convertToBase64 from './fileToBase64'
 const saveImages = async function (images) {
   let imageIds = []
   const byteSize = (str) => new Blob([str]).size
-  for (const base64Image in images) {
-    const imageMB = byteSize(images[base64Image]) / Math.pow(1024, 2)
+  for (const image in images) {
+    const base64image = images[image].getFileEncodeDataURL()
+    const imageMB = byteSize(base64image) / Math.pow(1024, 2)
     try {
       if (Math.round(imageMB) > 50) {
         throw new Error('Размерът на файла е твърде голям')
       }
       let savedImage = await api.post('pictures', {
-        image: images[base64Image],
+        image: base64image,
       })
       imageIds.push(savedImage.id)
     } catch (error) {
